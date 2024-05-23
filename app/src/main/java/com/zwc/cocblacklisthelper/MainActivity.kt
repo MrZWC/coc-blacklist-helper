4403da5c0b7518a4875592ed6ff750898a685bad
左唯辰 <zuoweichen@yineng.com.cn>
2024-05-23 18:06:54 +0800

    update

--- app/src/main/java/com/zwc/cocblacklisthelper/MainActivity.kt
+++ app/src/main/java/com/zwc/cocblacklisthelper/MainActivity.kt
@@ -2,10 +2,30 @@ package com.zwc.cocblacklisthelper
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuInflater
+import android.view.View
+import androidx.appcompat.widget.PopupMenu
+import com.zwc.cocblacklisthelper.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initListener()
+    }
+
+    private fun initListener() {
+        binding.menuBtn.setOnClickListener {
+            showMenu(it)
+        }
+    }
+
+    fun showMenu(view: View) {
+        val popup = PopupMenu(this, view)
+        val inflater: MenuInflater = popup.menuInflater
+        inflater.inflate(R.menu.main, popup.menu)
+        popup.show()
     }
 }
