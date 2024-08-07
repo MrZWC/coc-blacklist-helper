57fccba7fc4e9f0d524b523f37e3d4b1fb84df06
zuoweichen <675196884@qq.com>
2024-08-07 21:31:19 +0800

    发布版本 3.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.zwc.cocblacklisthelper"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 6
-        versionName = "3.0"
+        versionCode = 7
+        versionName = "3.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         flavorDimensions += listOf("default")
     }
